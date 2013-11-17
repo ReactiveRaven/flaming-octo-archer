9ec14fe0499f004f1b558db5a204c3d3b43ec7bd
ReactiveRaven <reactiveraven@gmail.com>
2013-11-17 21:42:05 +0000

    adding to register script to secure database

--- www/server/register.php
+++ www/server/register.php
@@ -1,5 +1,6 @@
 <?php
 
+// Set stuff up
 header("Content-Type: application/json");
 
 function jsondie($output, $debug = null) {
@@ -17,9 +18,10 @@ if (!isset($couchdb[$settingshost])) {
 }
 
 $couchdbsettings = $couchdb[$settingshost];
-
 include_once './CouchUser.php';
+$CouchUser = new CouchUser($couchdbsettings["url"], $couchdbsettings["adminuser"], $couchdbsettings["adminpass"]);
 
+// Get username and password, die on errors
 if (!isset($_REQUEST["username"])) {
     jsondie(array("forbidden" => "Must supply a 'username' argument"));
 }
@@ -28,8 +30,6 @@ if (!isset($_REQUEST["password"])) {
     jsondie(array("forbidden" => "Must supply a 'password' argument"));
 }
 $registerPassword = $_REQUEST["password"];
-
-$CouchUser = new CouchUser($couchdbsettings["url"], $couchdbsettings["adminuser"], $couchdbsettings["adminpass"]);
 $CouchUser->setUser($registerUsername, $registerPassword);
 
 $checkExistingUser = $CouchUser->becomeAdmin()->doGet("commissar_user_" . $registerUsername);
@@ -37,6 +37,7 @@ if ($checkExistingUser["response"]["status"]["status"] != 404) {
     jsondie(array("forbidden" => "User already exists"), $checkExistingUser);
 }
 
+// Set up the new user in the users database
 $userArray = array(
     "type" => "user",
     "_id" => "org.couchdb.user:" . $registerUsername,
@@ -44,13 +45,12 @@ $userArray = array(
     "password" => $registerPassword,
     "roles" => array()
 );
-
 $createNewUser = $CouchUser->doPut("_users/org.couchdb.user:" . $registerUsername, $userArray);
-
 if ($createNewUser["response"]["status"]["status"] > 299) {
     jsondie(array("error" => "Could not create user"), $createNewUser);
 }
 
+// Create the user's personal database
 $userDatabaseName = "commissar_user_" . $registerUsername;
 
 $createUserDatabase = $CouchUser->doPut($userDatabaseName, null);
@@ -59,6 +59,25 @@ if ($createUserDatabase["response"]["status"]["status"] > 299) {
     jsondie(array("error" => "Could not create user store"), $createUserDatabase);
 }
 
+// Set up the security object
+$security = array(
+    "admins" => array(
+        "names" => array(),
+        "roles" => array("+admin")
+    ),
+    "members" => array(
+        "names" => array($registerUsername),
+        "roles" => array()
+    )
+);
+
+$createSecurity = $CouchUser->doPut($userDatabaseName . "/_security", $security);
+
+if ($createSecurity["response"]["status"]["status"] > 299) {
+    jsondie(array("error" => "Could not secure your user account"), $createSecurity);
+}
+
+// Set up replication from global to personal
 $globalReplicationArray = array(
     "_id" => "user:" . $registerUsername . "_validation_global",
     "source" => $couchdbsettings["databases"]["validation_global"],
@@ -76,6 +95,7 @@ if ($globalValidationReplication["response"]["status"]["status"] > 299) {
     jsondie(array("error" => "Global validation not applied"), $globalValidationReplication);
 }
 
+// Set up replication from users to personal
 $userReplicationArray = array(
     "_id" => "user:" . $registerUsername . "_validation_users",
     "source" => $couchdbsettings["databases"]["validation_users"],
@@ -93,4 +113,5 @@ if ($userValidationReplication["response"]["status"]["status"] > 299) {
     jsondie(array("error" => "User validation not applied"), $userValidationReplication);
 }
 
+// OK, done :3
 jsondie(array("ok" => true));
