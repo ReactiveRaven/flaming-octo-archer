ca732938e42a84adcb09500e4239481419e1a60a
ReactiveRaven <reactiveraven@gmail.com>
2014-02-25 09:14:12 +0000

    thing
    Conflicts:
    	www/css/template.css
    	www/css/template.less

---
+++ www/angular/js/controllers/MessageCtrl.js
@@ -0,0 +1,48 @@
+/* global angular:false */
+var angular = angular;
+define(['../services/Authentication', '../filters/Capitalize', '../directives/LoginForm', '../services/ParanoidScope'], function () {
+    "use strict";
+    
+    var MenuCtrlModule = angular.module(
+        'commissar.controllers.MenuCtrl',
+        ['commissar.services.Authentication', 'commissar.directives.Markdown', 'commissar.filters.Capitalize', 'commissar.directives.LoginForm', 'commissar.services.ParanoidScope']
+    );
+    
+    MenuCtrlModule.controller('MenuCtrl', function ($scope, Authentication, ParanoidScope, $q) {
+        $scope.name = 'MenuCtrl';
+        
+        $scope.loggedIn = false;
+        $scope.isAdmin = false;
+        
+        $scope.onAuthChange = function () {
+            
+            $q.all(
+                [
+                    Authentication.loggedIn(),
+                    Authentication.getSession(),
+                    Authentication.isAdmin()
+                ]
+            ).then(
+                function (returnValues) {
+                    $scope.loggedIn = returnValues[0];
+                    $scope.userCtx = returnValues[1];
+                    $scope.isAdmin = returnValues[2];
+                    ParanoidScope.apply($scope);
+                    ParanoidScope.digest($scope);
+                }
+            );
+            
+        };
+        
+        $scope.onAuthChange();
+        
+        $scope.$on('AuthChange', $scope.onAuthChange);
+        
+        $scope.logout = function () {
+            Authentication.logout();
+        };
+        
+    });
+    
+    return MenuCtrlModule;
+});
